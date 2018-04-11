378ee2fb965a76604e1e23c58781ba85fbf1af58
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-04-11 14:31:40 +0100

    Simplifying REST API example

--- rest.js
+++ rest.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded())
 
 const notes = []
 
