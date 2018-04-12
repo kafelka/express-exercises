3dee083963f35d0e1add8975d8ee9a46dabf5c9a
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-04-12 12:30:38 +0100

    Adding some more challenges to the logger script

--- logger.js
+++ logger.js
@@ -2,7 +2,11 @@ const express = require("express")
 
 const app = express()
 
-app.use(/* Add your middleware here */)
+app.use(/* Add some middleware here that logs the time of EVERY request */)
+
+/* Add some middleware here that logs the time of GET requests only */
+
+/* Add some middleware here that logs the time POST requests only */
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!")
