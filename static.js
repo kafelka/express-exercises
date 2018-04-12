1f3a3423d3574072ae2e6fc2d3e5b82573293215
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-04-12 17:29:08 +0100

    Adding static files solution

--- static.js
+++ static.js
@@ -2,6 +2,6 @@ const express = require("express")
 
 const app = express()
 
-// Add code here for serving static files
+app.use(express.static("resources"))
 
 app.listen(4000, () => console.log("Listening for connections on port 4000"))
